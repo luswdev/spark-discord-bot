a5659de056951e8e122d8b76b0f1c93c4e3a8104
LuSkywalker <callumlu@gmail.com>
2024-03-21 17:17:31 +0800

    [M] make doc reader can read with custom path

--- utils/UtlDocReader.js
+++ utils/UtlDocReader.js
@@ -10,8 +10,12 @@ class DocReader {
         this.extinsion = 'md'
     }
 
-    read (_name) {
-        let path = `${join(this.basepath, _name)}.${this.extinsion}`
+    read (_name, _path = '') {
+        let basepath = this.basepath
+        if (_path) {
+            basepath = _path
+        }
+        let path = `${join(basepath, _name)}.${this.extinsion}`
         if (existsSync(path)) {
             return readFileSync(path).toString()
         }
@@ -20,4 +24,4 @@ class DocReader {
 }
 
 const docReader = new DocReader()
-module.exports.read = (_name) => docReader.read(_name)
+module.exports.read = (_name, _path = '') => docReader.read(_name, _path)
